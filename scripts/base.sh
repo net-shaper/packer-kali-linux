f01eb586f5cf8f776c7ba138a69047fa02721072
Alec Gleason <alec.gleason@neohapsis.com>
2017-11-29 22:58:40 -0600

    adding open-vm-tools

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,7 +6,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -qq dist-upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install linux-headers-$(uname -r)
 
 # VM Guest Support
-DEBIAN_FRONTEND=noninteractive apt-get -y -qq install open-vm-tools-desktop fuse iptables-persistent
+DEBIAN_FRONTEND=noninteractive apt-get -y -qq install open-vm-tools open-vm-tools-desktop fuse iptables-persistent
 
 # Cleanup
 apt-get -y autoclean
