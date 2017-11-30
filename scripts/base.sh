8e02bea7678c0416d86c725f31f6989ed9bed15d
Alec Gleason <alec.gleason@neohapsis.com>
2017-11-29 23:13:02 -0600

    Moving VM portions to standalone script

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,9 +5,6 @@ apt-get -y -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq dist-upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install linux-headers-$(uname -r)
 
-# VM Guest Support
-DEBIAN_FRONTEND=noninteractive apt-get -y -qq install open-vm-tools open-vm-tools-desktop fuse iptables-persistent
-
 # Cleanup
 apt-get -y autoclean
 apt-get -y clean
---
+++ scripts/vm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# VM Guest Support
+apt-get -y -qq install open-vm-tools open-vm-tools-desktop fuse iptables-persistent
