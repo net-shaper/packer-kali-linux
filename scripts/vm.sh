2ffd5c009b770b5340f8117bd000bb922ca0a3ae
Alec Gleason <alec.gleason@neohapsis.com>
2017-12-09 23:20:16 -0600

    removing iptables-persistent

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # VM Guest Support
-apt-get -y -qq install open-vm-tools open-vm-tools-desktop fuse iptables-persistent
+apt-get -y -qq install open-vm-tools open-vm-tools-desktop fuse
